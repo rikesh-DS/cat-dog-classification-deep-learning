e4097baefa9e6bda5beeec01b68d36c0233b976a
rikesh-DS <58325199+rikesh-DS@users.noreply.github.com>
2020-07-20 16:05:18 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -22,10 +22,20 @@ database={'nachi':'123','james':'aac','karthik':'asdsf'}
 def upload_image():
     if request.method == 'POST':
         if request.files:
-            image_path = request.files["image"] 
+            path = request.files["image"]
+            l = os.listdir('static')
+            for i in l:
+               file = 'static/'+i
+               os.remove(file)
+               
+            #print(path.read())            
             print('helloooo')
-            #image_path.save(os.path.join(app.config["IMAGE_UPLOADS"], image_path.filename))
-  
+            print(path.filename)
+            print('hellooooname')
+            #os.remove('./static/*.*')
+            path.save(os.path.join(app.config["IMAGE_UPLOADS"], path.filename))
+            filepath = 'static/'+path.filename
+            print(filepath)
             #data1 = np.expand_dims(data, axis=0)
             #data1 = data1 * 1.0 / 255
             #filename = secure_filename(image.filename)
@@ -39,19 +49,23 @@ def upload_image():
             #    output = 'DOG'
             #elif prediction == 0:
             #    output = 'CAT'
-            img_array1  = image.load_img("15.jpg", target_size=(50, 50),grayscale=True)
+            img_array1  = image.load_img(filepath, target_size=(50, 50),color_mode = "grayscale")
             data1 = np.array(img_array1).reshape(-1,IMG_SIZE,IMG_SIZE,1)
             #img_array1 = cv2.imread(image,cv2.IMREAD_GRAYSCALE)
             #new = cv2.resize(img_array1,(IMG_SIZE,IMG_SIZE))
             #new1 = new.reshape(-1,IMG_SIZE,IMG_SIZE,1)
-             
+            print('im hello before model**************************************************')
+            data1 = tf.cast(data1, tf.float32)
             prediction = model.predict(data1)
-           
+            print('im hello before model22222**************************************************')
             if prediction[0] == 1:
                 predict_value = 'DOG'
             elif prediction[0] == 0:
                 predict_value = 'CAT'
                 
+            print('veluuuuuuuuuuuu')
+            print(prediction)
+            print('im hello before model*****444444*********************************************')   
             print('im hello')
             #im1 = im1.save("geeks.jpg") 
             #print(filename)
@@ -61,13 +75,13 @@ def upload_image():
             #cv2.waitKey(0)
             #cv2.destroyAllWindows()
             #plt.show()
-            predict_value = 'Dog'
+            #predict_value = 'Dog'
             #file.save(os.path.join(app.config['IMAGE_UPLOADS'],filename))
             
             #image.save(secure_filename(image.filename))
             #image.save(os.path.join(app.config["IMAGE_UPLOADS"]), image.filename)
             print("image saved")
-            return render_template('login.html', prediction_text='Animal is  $ {}'.format(predict_value))
+            return render_template('login.html', prediction_text='Animal is  $ {}'.format(predict_value),test = filepath)
 
 if __name__ == '__main__':
     app.run()
